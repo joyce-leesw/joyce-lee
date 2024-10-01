bf00df45739380f672287b4cd68729dee54b3dc4
Joyce Lee <shuwei1203@gmail.com>
2024-10-01 21:38:19 +0100

    added website icon

---
+++ src/assets/website-icon.jsx
@@ -0,0 +1,27 @@
+const WebsiteIcon = (props) => (
+	<svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={1.5}
+    {...props}
+  >
+    <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+    <g
+      id="SVGRepo_tracerCarrier"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <g id="SVGRepo_iconCarrier">
+      <polyline points="8.25 2.75,2.75 2.75,2.75 13.25,13.25 13.25,13.25 7.75" />
+      <path d="m13.25 2.75-5.5 5.5m3-6.5h3.5v3.5" />
+    </g>
+  </svg>
+);
+
+export default WebsiteIcon;
