25339716ac2f6c20018c55ec8dead14a8b73820d
Joyce Lee <shuwei1203@gmail.com>
2024-10-02 18:20:47 +0100

    added white to the text

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -2,7 +2,13 @@
 export default function TabButton({ company, isSelected, ...props }) {
 	return (
 		<li>
-			<button className={isSelected ? 'selected' : ''} {...props}>{company}</button>
+			<button 
+				className={isSelected ? 'selected' : ''} 
+				style={{ color: 'white' }}
+				{...props}
+			>
+				{company}
+			</button>
 		</li>
 	)
 }
