4f026efbe94856087f3f9437c6567c24d137d1f3
Joyce Lee <shuwei1203@gmail.com>
2025-01-14 22:34:42 +0000

    updated resume

--- src/data.js
+++ src/data.js
@@ -1,46 +1,62 @@
 export const INTRO_TITLE = "Hello World! I'm";
-export const INTRO_NAME = "Joyce Lee"
-export const INTRO_SUBTITLE = "I turn random ideas into things, one spark at a time.";
-export const ABOUT = `I'm a software engineer based in *London*, currently working at <a href="https://postop.ai" class="highlight hidden-link">Post Op</a>, where I design intuitive mobile apps to make healthcare accessible to everyone, everywhere.\n\nOutside of work, I dive into the fascinating world of AI, where I experiment with different machine learning models to unlock the potential of data.\n\nWhen I'm not immersed in coding, I enjoy playing a game of chess, reading a good book, or hiking up a mountain where I question all my life decisions around the third mile.`;
+export const INTRO_NAME = 'Joyce Lee';
+export const INTRO_SUBTITLE =
+  'I bring ideas to life through code, one spark at a time.';
+export const ABOUT = `I'm a software engineer based in *London*, currently working at <a href="https://postop.ai" class="highlight hidden-link">Post Op</a>, where I design intuitive mobile apps to make healthcare accessible to everyone, everywhere.\n\nOutside of work, I dive into the fascinating world of AI, where I experiment with different machine learning models to explore the potential of data.\n\nWhen I'm not immersed in coding, I enjoy playing a game of chess, reading a good book, or hiking up a mountain where I question all my life decisions around the third mile.`;
 
 export const EXPERIENCES = [
-	{
-		company: 'Post Op',
-		title: 'Software Engineer',
-		date: 'JUNE 2023 - PRESENT',
-		achievements: ['- Collaborated closely with a small team to gradually rewrite an existing live production *React Native/Expo* mobile app while introducing new features simultaneously in *TypeScript*', '- Optimised a *Ruby-based* web backend, utilised *ERB* templating for frontend views, and integrated *JavaScript* for dynamic client-side interactivity; conducted rigorous testing throughout the application']
-	},
-	{
-		company: 'Lankester Engineering',
-		title: 'Machine Learning Engineer',
-		date: 'OCT 2022 - APR 2023',
-		achievements: ['- Designed and implemented a system in *Python* utilising *Tensorflow* and *Keras*, integrating *LSTM with an autoencoder* and *DNN* to detect operational anomalies in IoT devices and predict equipment lifecycle based on historical performance data', '- Successfully predicted the state-of-health(SoH) and state-of-charge(SoC) over charging cycles and end-of-life(EOL) of batteries; productionised the models by creating *REST API* using *Django*']
-	}
-]
+  {
+    company: 'Post Op',
+    title: 'Software Engineer',
+    date: 'JUNE 2023 - PRESENT',
+    achievements: [
+      '- Collaborated closely with a small team to complete redesign and feature expansion in *TypeScript*, resulting in a successful user launch',
+      '- Successfully transitioned an *Expo-based* MVP to a production-ready mobile application',
+      '- Refactored codebase and reduced latency by 260% by optimising network requests and UI performance',
+      '- Introduced gamification within a mobile application to enhance user engagement, currently conducting research on A/B testing methodologies to optimise user experience and feature effectiveness',
+    ],
+  },
+  {
+    company: 'Lankester Engineering',
+    title: 'Machine Learning Engineer',
+    date: 'OCT 2022 - APR 2023',
+    achievements: [
+      '- Designed and implemented a system in *Python* utilising *Tensorflow* and *Keras*, integrating LSTM with an autoencoder and DNN to detect operational anomalies in IoT devices',
+      '- Successfully predicted the state-of-health(SoH) and state-of-charge(SoC) over charging cycles and end-of-life(EOL) of batteries with 92% accuracy on test data',
+      '- Productionised the models by creating REST API using *Django*',
+    ],
+  },
+];
 
 export const PROJECTS = [
-	{
-		title: 'Eventra',
-		description: 'Recommendation system using Machine Learning',
-		languages: ['Python', 'FastAPI', 'React', 'PostgreSQL'],
-		githubLink: 'https://github.com/joyce-leesw/eventra-frontend'
-	},
-	{
-		title: 'Chess Chatbot',
-		description: 'Analysed chess games and thoeries given any chess books',
-		languages: ['Python', 'Praw', 'LLamaIndex'],
-		githubLink: 'https://github.com/joyce-leesw/Sentiment-Analysis-of-ChatGPT-on-Reddit'
-	},
-	{
-		title: 'Tic Tac Toe',
-		description: 'Tic Tac Toe using Q-Learning',
-		languages: ['Python', 'E-Greedy Algorithm'],
-		githubLink: 'https://github.com/joyce-leesw/Tic-Tac-Toe-Bot-using-Q-Learning'
-	},
-]
+  {
+    title: 'Eventra',
+    description:
+      "A recommendation system that predicts user's interest in events based on past preferences",
+    languages: ['Python', 'FastAPI', 'TypeScript', 'PostgreSQL'],
+    websiteLink: 'https://github.com/joyce-leesw/eventra-frontend',
+    githubLink: 'https://github.com/harrywiner/eventra-backend',
+  },
+  {
+    title: 'Tic Tac Toe',
+    description:
+      'An interactive Tic Tac Toe interface featuring an AI bot trained using Reinforcement Learning',
+    languages: ['Python', 'React', 'JavaScript', 'E-Greedy Algorithm'],
+    websiteLink: 'https://github.com/joyce-leesw/Tic-Tac-Toe-Frontend',
+    githubLink:
+      'https://github.com/joyce-leesw/Tic-Tac-Toe-Bot-using-Q-Learning',
+  },
+  {
+    title: 'Encoder Decoder Translation',
+    description: 'A French-to-English language translation model',
+    languages: ['Tensorflow', 'Keras', 'Embeddings'],
+    githubLink:
+      'https://github.com/joyce-leesw/Sentiment-Analysis-of-ChatGPT-on-Reddit',
+  },
+];
 
-export const OUTRO_TITLE = 'Designed and built by Joyce Lee.'
-export const OUTRO_SUBTITLE = 'All rights reserved. ©'
+export const OUTRO_TITLE = 'Designed and built by Joyce Lee.';
+export const OUTRO_SUBTITLE = 'All rights reserved. ©';
 
-export const LINKEDIN_URL = 'https://www.linkedin.com/in/joyce-lee-259369120/'
-export const GITHUB_URL = 'https://github.com/joyce-leesw'
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/joyce-lee-259369120/';
+export const GITHUB_URL = 'https://github.com/joyce-leesw';
